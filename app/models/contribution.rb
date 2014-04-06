f0da932079d70a06d7ac31958b6ddf527c59517f
Zach Whelchel <zach.whelchel@gmail.com>
2014-04-06 15:53:00 -0500

    Added subscription capability and lots more.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -10,10 +10,32 @@ class Contribution < ActiveRecord::Base
   attr_accessible :amount, :amount_cents, :stripe_token, :stripe_currency
 
   def process_payment
-    charge = Stripe::Charge.create(amount: amount_cents,
-                                   currency: @stripe_currency,
-                                   card: @stripe_token,
-                                   description: "This is the description.")
+
+    if self.user.stripe_customer_id
+      # they already have a subscription or have made a onetime gift.
+      customer_id = self.user.stripe_customer_id
+
+      # check our db to see if they have a subscription
+
+      # cancel it... make a new one...
+
+
+
+    else 
+      customer = Stripe::Customer.create(
+        :card => @stripe_token,
+        :description => self.user.email,
+        :plan => "khalpar_1000"
+      )
+      customer_id = customer.id
+
+      self.user.update_attributes(:stripe_customer_id => customer_id)
+    end
+
+    # charge = Stripe::Charge.create(amount: amount_cents,
+    #                                currency: @stripe_currency,
+    #                                :customer => customer_id,
+    #                                description: "This is the description.")
     true
   rescue Stripe::CardError
     false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140406181621) do
+ActiveRecord::Schema.define(:version => 20140406195242) do
 
   create_table "contributions", :force => true do |t|
     t.datetime "created_at",   :null => false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20140406181621) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
