8d76a5b3da08e78a87234948aaf21bd7e1684ff5
Melanie Hall <melaniehalldesign@gmail.com>
2014-04-06 12:31:54 -0500

    New rails app with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneLifePayments::Application.config.secret_token = '9be3ff1ef0ad3b00ecfabb363c9336c144c8e21819078868110a0fca0aa858b539fa1ade55bc5251209a36991362bd4fa068d3b57a9b304b36820807ea0212e7'
