3645a6d8ed2811f372cf51223b8f24097ccef0b8
Melanie Hall <melaniehalldesign@gmail.com>
2014-06-20 22:37:33 -0500

    bumping asset version

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module OneLifePayments
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
     # Added for Devise.
     config.assets.initialize_on_precompile = false
